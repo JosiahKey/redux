22a5d636f48dae007d42407fd9e02d098af3f941
JosiahKey <josiah.key@gmail.com>
2025-11-04 17:35:42 -0500

    equipment_slot.gd
    fixed swap so that it checks before swapping

--- Version_0.0.2/Scripts/equipment_slot.gd
+++ Version_0.0.2/Scripts/equipment_slot.gd
@@ -5,7 +5,11 @@ enum Type {HEAD, NECK, SHOULDERS, MAINHAND, OFFHAND, RING1, RING2, WAIST, LEGS, 
 
 func _can_drop_data(_at_position: Vector2, data: Variant) -> bool:
 	if type == Type.GENERAL: #if the slot type is 'general', then drop anything if its empty
-		return true
+		if get_child_count() == 0:
+			return true
+		else: if type == data.get_parent().type:
+			return true
+		return get_child(0).type == data.type
 	else:
 		return data.type == type #return true if type of item and slot are the same
 
