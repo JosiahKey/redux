989ae555d7105628b8821e13f0f7ec9dc6d8f908
JosiahKey <josiah.key@gmail.com>
2025-11-04 17:47:34 -0500

    equip_panel.gd
    updated to pull initial items from player data global. similar to inventory

--- Version_0.0.2/Scripts/equip_panel.gd
+++ Version_0.0.2/Scripts/equip_panel.gd
@@ -1,7 +1,19 @@
-
 extends NinePatchRect
 
+var template_inv_slot = preload("res://Version_0.0.2/Scenes/Templates/inventory_slot.tscn")
 @onready var grid_ref := $GridContainer
+
+func _ready() -> void:
+	for i in PlayerData.equipment_data.keys():
+		var equip_slot_new = template_inv_slot.instantiate()
+		if PlayerData.equipment_data[i] != null:
+			var item_name  = GameData.item_data[str(int(PlayerData.equipment_data[i]))]["name"]
+			var icon_texture =  load("res://Version_0.0.2/Assets/item_assets/"+ item_name +".png")
+			equip_slot_new.get_node("Icon").set_texture(icon_texture)
+			equip_slot_new.get_node("Icon").custom_init(GameData.item_data[
+				str(int(PlayerData.equipment_data[i]))]["equipmentSlot"])
+			print(equip_slot_new.get_node("Icon").type)
+		grid_ref.add_child(equip_slot_new, true)
 
 #func get_equipped_items() -> Array[Variant]:
 	#var equipped_item_list: Array[Variant]
